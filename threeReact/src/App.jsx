ff9653c9ae9594efd0f52a52ac926bc706624e29
samrat-chauhan24 <chauhansamrat835@gmail.com>
2026-03-06 16:09:34 +0530

    made bg color changer using onclick and useState

--- threeReact/src/App.jsx
+++ threeReact/src/App.jsx
@@ -1,25 +1,37 @@
-import Card from './comps/Card.jsx'
+import { useState } from "react"
+// function setColor() {
 
-let myNewObj = {
-  name: "samrat",
-  surname:"chauhan",
-  age: 21
-}
-let myNewObj2 = {
-  name: "Ronak",
-  surname:"malik",
-  age: 21
-}
-let newArr = [1,2,24]
+// };
 function App() {
+  const [color, setColor] = useState("white")
   return (
-    <>
-    <h1 class="text-3xl font-bold underline bg-green-300 rounded-2xl  w-100 text-center">Samrat Chauhan</h1>
-      <Card anotherObj = {newArr} anotherArr = {myNewObj} platform="LeetCode"/> 
-      <Card anotherObj = {newArr} anotherArr = {myNewObj2} platform="github"/> 
-
-    </>
-  )
+    <div className="min-h-screen" style={{backgroundColor:color}}>
+      <h1 className="m-auto w-[50vw] text-center">Color changer project</h1>
+      <div className="divOfButtons flex w-[90vw] bg-gray-500 justify-evenly rounded-sm m-auto relative top-60">
+        <button
+          onClick={()=>setColor("red")} 
+          className="text-xs button-col bg-red-500  text-amber-50 rounded-sm m-1 w-[8vw]">Red</button>
+        <button
+          onClick={()=>setColor("blue")}  
+          className="text-xs button-col bg-blue-500  text-amber-50 rounded-sm m-1 w-[8vw]">Blue</button>
+        <button 
+          onClick={()=>setColor("green")} 
+          className="text-xs button-col bg-green-500  text-amber-50 rounded-sm m-1 w-[8vw]">Green</button>
+        <button 
+          onClick={()=>setColor("yellow")} 
+          className="text-xs button-col bg-yellow-500  text-amber-50 rounded-sm m-1 w-[8vw]">Yellow</button>
+        <button 
+          onClick={()=>setColor("black")} 
+          className="text-xs button-col bg-black text-amber-50 rounded-sm m-1 w-[8vw]">Black</button>
+        <button 
+          onClick={()=>setColor("pink")} 
+          className="text-xs button-col bg-pink-500 text-amber-50 rounded-sm m-1 w-[8vw]">Pink</button>
+        <button
+          onClick={()=>setColor("cyan")}  
+          className="text-xs button-col bg-cyan-500 text-amber-50 rounded-sm m-1 w-[8vw]">Cyan</button>
+      </div>
+    </div>// we can use on click also to change the color
+  ) 
 }
 
-export default App
+export default App 
