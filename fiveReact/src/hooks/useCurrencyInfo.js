2df4844aab391bac31bef3b66b8d743a97b570ab
samrat-chauhan24 <chauhansamrat835@gmail.com>
2026-03-08 18:23:21 +0530

    made a currency calculator, used custom hooks, reusable comps

---
+++ fiveReact/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({}) 
+    useEffect(() => {
+        if(!currency) return;
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then(res => setData(res[currency])) 
+    }, [currency])
+    
+    return data;
+}
+
+export default useCurrencyInfo
