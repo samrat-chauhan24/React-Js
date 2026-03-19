bf76cbd13c6c61e037e714966b797b57fb1b6a3f
samrat-chauhan24 <chauhansamrat835@gmail.com>
2026-03-19 14:54:55 +0530

    used the another syntax of context api and made a dark and light theme switch

---
+++ seven2React/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import { ThemeProvider } from "./contexts/Theme"
+import ThemeBtn from "./components/ThemeBtn"
+import Card from "./components/Card"
+
+function App() {
+  const [themeMode, setThemeMode] = useState("dark")
+  const lightTheme = () => {setThemeMode("light")}
+  const darkTheme = () => {setThemeMode("dark")}
+
+  // actual code to change the theme
+  useEffect(() => {
+  document.documentElement.classList.toggle(
+    "dark",
+    themeMode === "dark"
+  )
+}, [themeMode])
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn/>
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
